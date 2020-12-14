6417137dc2c2ec9711ee701d33474dfd0663488e
longmaoaiali <13213102491@163.com>
2020-12-14 17:17:43 +0800

    init Android ffmpeg project

---
+++ testFFmpeg/app/src/main/java/com/cvte/testffmpeg/MainActivity.java
@@ -0,0 +1,63 @@
+package com.cvte.testffmpeg;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] permissions =  new String[]{
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+    };
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    private int mRequestCode = 664;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //获取读写权限
+        requestUserPermissions();
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+
+        open("/sdcard/1080.mp4",this);
+    }
+
+    private void requestUserPermissions() {
+        requestPermissions(permissions,mRequestCode);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode==mRequestCode) {
+            for (int grantResult : grantResults) {
+                if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "获取读写权限失败", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+
+            Toast.makeText(this, "已获取读写权限", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+    public native boolean open(String url,Object handle);
+}
