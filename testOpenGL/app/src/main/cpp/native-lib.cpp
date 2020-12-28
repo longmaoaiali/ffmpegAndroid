8c315d79791abb5d2d729bfbcbf64950d32c5817
longmaoaiali <13213102491@163.com>
2020-12-28 23:08:46 +0800

    modify shader GPU code

--- testOpenGL/app/src/main/cpp/native-lib.cpp
+++ testOpenGL/app/src/main/cpp/native-lib.cpp
@@ -12,33 +12,34 @@
 //#可以将字符转换为字符串
 #define GET_STR(x) #x
 static const char *vertexShader = GET_STR(
-    attribute vec4 aPosition;//定点坐标
-    attribute vec2 aTexCoord;//材质顶点坐标
-    varying vec2 vTexCoord;  //输出的材质坐标
-    void main(){
-        vTexCoord=vec2(aTexCoord.x,1.0-aTexCoord.y);
-        gl_Position = aPosition;
-    }
+        attribute vec4 aPosition; //顶点坐标
+        attribute vec2 aTexCoord; //材质顶点坐标
+        varying vec2 vTexCoord;   //输出的材质坐标
+        void main(){
+            vTexCoord = vec2(aTexCoord.x,1.0-aTexCoord.y);
+            gl_Position = aPosition;
+        }
 );
 
+//片元着色器,软解码和部分x86硬解码
 static const char *fragYUV420P = GET_STR(
-    precision mediump float; //精度
-    varying vec2 vTexCoord;  //顶点着色器传递的坐标
-    uniform sampler2D yTexture; //输入的材质
-    uniform sampler2D uTexture;
-    uniform sampler2D vTexture;
-    void main(){
-        vec3 yuv;
-        vec3 rgb;
-        yuv.r = texture2D(yTexture,vTexture).r;
-        yuv.g = texture2D(uTexture,vTexture).r - 0.5;
-        yuv.b = texture2D(vTexture,vTexture).r - 0.5;
-        rgb = mat3(1.0,     1.0,    1.0,
-                  0.0,-0.39465,2.03211,
-                  1.13983,-0.5806,0.0)*yuv;
-        //输出像素颜色
-        gl_FragColor = vec4(rgb,1,0);
-    }
+        precision mediump float;    //精度
+        varying vec2 vTexCoord;     //顶点着色器传递的坐标
+        uniform sampler2D yTexture; //输入的材质（不透明灰度，单像素）
+        uniform sampler2D uTexture;
+        uniform sampler2D vTexture;
+        void main(){
+            vec3 yuv;
+            vec3 rgb;
+            yuv.r = texture2D(yTexture,vTexCoord).r;
+            yuv.g = texture2D(uTexture,vTexCoord).r - 0.5;
+            yuv.b = texture2D(vTexture,vTexCoord).r - 0.5;
+            rgb = mat3(1.0,     1.0,    1.0,
+                       0.0,-0.39465,2.03211,
+                       1.13983,-0.58060,0.0)*yuv;
+            //输出像素颜色
+            gl_FragColor = vec4(rgb,1.0);
+        }
 );
 
 GLint InitShader(const char *code,GLint type)
